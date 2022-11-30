de901dfeb81593b2379266aa039d1a3705cca1f8
Eloi Amyot <eloi.amyot@gmail.com>
2022-11-30 10:54:51 -0500

    Début de projet

--- app/src/main/java/com/example/chessclock/MainActivity.java
+++ app/src/main/java/com/example/chessclock/MainActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int time1 = 200;
+    int time2 = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.setTimer){
+        if(item.getItemId() == R.id.settings){
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
+        }
+        if(item.getItemId() == R.id.reset){
+            time1 = 120;
+            Button button1 = findViewById(R.id.button1);
+            button1.setText(String.valueOf(time1) + " s");
+            time2 = 120;
+            Button button2 = findViewById(R.id.button2);
+            button2.setText(String.valueOf(time2) + " s");
         }
         return super.onOptionsItemSelected(item);
     }
