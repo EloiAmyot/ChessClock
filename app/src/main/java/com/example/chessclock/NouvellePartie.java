6d7578d28879b25e0e14161a998ddde8d3150a87
Eloi Amyot <eloi.amyot@gmail.com>
2022-12-23 15:12:59 -0500

    Favorites

--- app/src/main/java/com/example/chessclock/NouvellePartie.java
+++ app/src/main/java/com/example/chessclock/NouvellePartie.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class NouvellePartie extends AppCompatActivity {
 
@@ -23,27 +24,25 @@ public class NouvellePartie extends AppCompatActivity {
         Intent intent  = new Intent(getIntent());
         Button boutton;
         boutton =findViewById(R.id.boutton);
+        RadioButton radiobutton = findViewById(R.id.radioButton);
         if(intent.getStringExtra("origine").equals("nouveau_preset")){
-            Button radiobutton = findViewById(R.id.radioButton);
             radiobutton.setEnabled(false);
-
-
+            radiobutton.setChecked(true);
             boutton.setText("Confirmer");
             boutton.setOnClickListener(view -> {
                 EditText time1 = findViewById(R.id.temps1);
                 EditText increment1 = findViewById(R.id.newIncrement1);
                 EditText time2 = findViewById(R.id.temps2);
                 EditText increment2 = findViewById(R.id.newIncrement2);
+                preset.setFavorite(true);
                 if (!time1.getText().toString().equals("") && !increment1.getText().toString().equals("") && !time2.getText().toString().equals("") && !increment2.getText().toString().equals("")) {
-                    preset.setFavorite(true);
+
                     preset.setTime1(Integer.parseInt(time1.getText().toString()));
                     preset.setIncrement1(Integer.parseInt(increment1.getText().toString()));
                     preset.setTime2(Integer.parseInt(time2.getText().toString()));
                     preset.setIncrement2(Integer.parseInt(increment2.getText().toString()));
                 }
-                if (preset.getFavorite()){
-                    model.addData(preset);
-                }
+                model.addData(preset);
                 Intent allo = new Intent();
                 allo.putExtra("Preset", preset);
                 this.setResult(this.RESULT_OK, allo);
@@ -57,13 +56,15 @@ public class NouvellePartie extends AppCompatActivity {
                 EditText time2 = findViewById(R.id.temps2);
                 EditText increment2 = findViewById(R.id.newIncrement2);
                 if (!time1.getText().toString().equals("") && !increment1.getText().toString().equals("") && !time2.getText().toString().equals("") && !increment2.getText().toString().equals("")) {
-                    preset.setFavorite(true);
                     preset.setTime1(Integer.parseInt(time1.getText().toString()));
                     preset.setIncrement1(Integer.parseInt(increment1.getText().toString()));
                     preset.setTime2(Integer.parseInt(time2.getText().toString()));
                     preset.setIncrement2(Integer.parseInt(increment2.getText().toString()));
                 }
-
+                if (radiobutton.isChecked()){
+                    preset.setFavorite(true);
+                    model.addData(preset);
+                }
                 Intent returnIntent = new Intent();
                 returnIntent.putExtra("Preset", preset);
                 setResult(RESULT_OK, returnIntent);
